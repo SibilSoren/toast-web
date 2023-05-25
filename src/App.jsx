ca1f85545a61971e89786687661a7324d2a1c6b2
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2023-05-26 00:38:27 +0530

    Tailwind package and React loader package installed ,along with login page created

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import SignIn from "./Pages/SignIn/SignIn";
 
 function App() {
-  let isSignedIn = false;
+  let isSignedIn = true;
   let navigate = useNavigate();
   useEffect(() => {
     if (!isSignedIn) {
@@ -17,14 +17,15 @@ function App() {
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/app" element={<h1>App Page</h1>} />
       </Routes>
-      <button
+      {/* <button
         onClick={() => {
           navigate("/about");
         }}
       >
         Navigate
-      </button>
+      </button> */}
     </>
   );
 }
--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-
+import { useNavigate } from "react-router-dom";
+import { Oval } from "react-loader-spinner";
 function SignIn() {
+  let navigate = useNavigate();
+  let [userNameError, setUserNameError] = useState(false);
+  let [emailError, setEmailError] = useState(false);
+  let [loading, setLoading] = useState(false);
   const {
     register,
     handleSubmit,
@@ -8,23 +14,88 @@ function SignIn() {
   } = useForm();
   const submitHandler = (formData) => {
     console.log(formData);
+    if (formData.username === "") {
+      setUserNameError(true);
+    }
+    if (formData.email === "") {
+      setEmailError(true);
+    }
+    if (formData.username && formData.email) {
+      // navigate("/app");
+      setLoading(true);
+    }
   };
   return (
-    <>
-      <form onSubmit={handleSubmit(submitHandler)}>
-        <label htmlFor="username">User name</label>
-        <input placeholder="Tom" {...register("username")} />
-        <label htmlFor="email">Email</label>
-        <input
-          placeholder="mail@mail.com"
-          {...register("email", {
-            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-          })}
-        />
-        {errors?.email?.type === "pattern" && <p>Enter valid email</p>}
-        <input type="submit" />
-      </form>
-    </>
+    <div className="h-screen flex justify-center items-center">
+      <div className="border-2 border-slate-700 w-2/6 h-fit p-5 rounded-lg shadow-2xl">
+        <h2 className="text-center w-full text-2xl font-bold m-2 mb-4">
+          Login
+        </h2>
+        <form onSubmit={handleSubmit(submitHandler)} className="flex flex-col">
+          <label htmlFor="username" className="font-semibold text-lg mb-2">
+            User name
+          </label>
+          <input
+            className={`border-2 ${
+              userNameError ? "border-rose-800" : "border-slate-700"
+            }  h-12 rounded-md p-2 mb-4`}
+            placeholder="Tom"
+            {...register("username", {
+              onBlur: (e) => {
+                if (e.target.value !== "") {
+                  setUserNameError(false);
+                } else {
+                  setUserNameError(true);
+                }
+              },
+            })}
+          />
+          <label htmlFor="email" className="font-semibold text-lg mb-2">
+            Email
+          </label>
+          <input
+            className={`border-2 ${
+              emailError ? "border-rose-800" : "border-slate-700"
+            } h-12 rounded-md p-2 mb-1`}
+            placeholder="mail@gmail.com"
+            {...register("email", {
+              pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            })}
+          />
+          {errors?.email?.type === "pattern" && (
+            <p className="text-rose-600">Please enter valid email id</p>
+          )}
+          {/* <input type="submit" className="rounded-lg bg-indigo-500 p-4" /> */}
+
+          <button
+            type="submit"
+            className={`bg-slate-600 ${
+              loading ? "w-2/5" : "w-1/5"
+            }  m-auto mt-3 h-12 rounded-lg text-white hover:bg-slate-800 active:bg-slate-800 focus:outline-none focus:ring focus:ring-violet-100 transition duration-500 ease-in`}
+          >
+            {loading ? (
+              <div className="flex items-center ">
+                <Oval
+                  height={25}
+                  width={25}
+                  color="#FFFFFF"
+                  wrapperStyle={{}}
+                  wrapperClass="ml-2"
+                  visible={true}
+                  ariaLabel="oval-loading"
+                  secondaryColor="#d7d7d9"
+                  strokeWidth={4}
+                  strokeWidthSecondary={5}
+                />
+                <p className="ml-3">Processing...</p>
+              </div>
+            ) : (
+              "Submit"
+            )}
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
